3600b9146ffdd1d2efc05efb12a0e0428978c366
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-06-15 12:29:40 +0700

    fix: comment out unused "Artikel" menu item in SidebarMenu

--- resources/js/Components/Admin/SideBar/SidebarMenu.jsx
+++ resources/js/Components/Admin/SideBar/SidebarMenu.jsx
@@ -26,11 +26,11 @@ export default function SidebarMenu() {
             routeName: "kuis.admin",
             icon: "kuis",
         },
-        {
-            label: "Artikel",
-            routeName: "artikel-admin.index",
-            icon: "artikel",
-        },
+        // {
+        //     label: "Artikel",
+        //     routeName: "artikel-admin.index",
+        //     icon: "artikel",
+        // },
 
     ];
 
