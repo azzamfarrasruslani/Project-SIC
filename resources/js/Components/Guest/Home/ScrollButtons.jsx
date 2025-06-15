185dd70a85dd030fcd1f3b4b8147bcb2c2878ae5
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-06-15 12:52:17 +0700

    refactor: improve formatting and consistency in ScrollButtons component

--- resources/js/Components/Guest/Home/ScrollButtons.jsx
+++ resources/js/Components/Guest/Home/ScrollButtons.jsx
@@ -1,22 +1,22 @@
-
 import Icon from "@/Components/Icon/Icon";
+
 const ScrollButtons = ({ onTop, onBottom }) => {
     return (
         <div className="fixed bottom-6 right-6 flex flex-col space-y-3 z-50">
             <button
                 onClick={onTop}
-                className=" border-2 border-lime-700 text-lime-700 h-12 w-12 rounded-full shadow  transition-all duration-200 transform hover:scale-105"
+                className="flex items-center justify-center border-2 border-lime-700 text-lime-700 h-12 w-12 rounded-full shadow transition-all duration-200 transform hover:scale-105"
             >
-                <Icon name="arrowUp"></Icon>
+                <Icon name="arrowUp" />
             </button>
             <button
                 onClick={onBottom}
-                 className=" border-2 border-lime-700 text-lime-700 h-12 w-12 rounded-full shadow transition-all duration-200 transform hover:scale-105"
+                className="flex items-center justify-center border-2 border-lime-700 text-lime-700 h-12 w-12 rounded-full shadow transition-all duration-200 transform hover:scale-105"
             >
-                {" "}
-                <Icon name="arrowDown"></Icon>
+                <Icon name="arrowDown" />
             </button>
         </div>
     );
 };
+
 export default ScrollButtons;
