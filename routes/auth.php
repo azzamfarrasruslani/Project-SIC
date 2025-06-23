4e624c728b9c3ff62815873a7d35ace01e12dde2
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-06-23 22:16:59 +0700

    fix: add authentication and verification middleware to register route

--- routes/auth.php
+++ routes/auth.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+        ->name('register')->middleware(['auth', 'verified']);
 
     Route::post('register', [RegisteredUserController::class, 'store'])->middleware(['auth', 'verified']);
 
