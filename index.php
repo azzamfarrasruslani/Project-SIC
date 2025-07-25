c12326c05743a769be949233871be6272db07872
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-07-25 15:42:21 +0700

    fix: update SCRIPT handling and bind public path in index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Tambahkan ini
+// Fix untuk SCRIPT info
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/index.php';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
@@ -17,4 +17,18 @@ require __DIR__.'/vendor/autoload.php';
 
 $app = require_once __DIR__.'/bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Tambahan penting agar public_path() jadi __DIR__
+$app->bind('path.public', function () {
+    return __DIR__;
+});
+
+// Handle request Laravel standar
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
