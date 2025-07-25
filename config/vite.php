67f60d66e4154b430b848a5704f80a2981bfc716
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-07-25 15:25:06 +0700

    feat: add Vite configuration for asset build path

---
+++ config/vite.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Vite Asset Build Path
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the directory where the Vite build files including
+    | the manifest file will be located. By default, Laravel assumes the
+    | build directory is located at "public/build", but you can change it.
+    |
+    */
+
+    'build_directory' => 'build',
+
+];
