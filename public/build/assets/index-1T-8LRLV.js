5f66da9bdf3c495bf32a0a9bad095b5ce468cf3f
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-08-17 10:12:36 +0700

    build

---
+++ public/build/assets/index-1T-8LRLV.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DXBGwqw_.js";import{K as t}from"./KuisCard-fk-Ld998.js";const o=({kuis:r})=>s.jsx("div",{className:"",children:s.jsx(t,{kuis:r})});export{o as default};
--- public/build/assets/index-Cn1Sl6XG.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-BGWcB_pq.js";import{K as t}from"./KuisCard-xcOizLhd.js";const o=({kuis:r})=>s.jsx("div",{className:"",children:s.jsx(t,{kuis:r})});export{o as default};
