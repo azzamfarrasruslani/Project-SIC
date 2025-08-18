3609677f3f1dd26077b7144543a6d6264ec18523
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2025-08-18 22:51:52 +0700

    build

--- build/assets/index-1Vy7jYxa.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-Dh9U4SK1.js";import{K as t}from"./KuisCard-Bltp7wdS.js";const o=({kuis:r})=>s.jsx("div",{className:"",children:s.jsx(t,{kuis:r})});export{o as default};
---
+++ build/assets/index-DhU9yHNf.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-CLafzsQr.js";import{K as t}from"./KuisCard-CW44yoNd.js";const o=({kuis:r})=>s.jsx("div",{className:"",children:s.jsx(t,{kuis:r})});export{o as default};
